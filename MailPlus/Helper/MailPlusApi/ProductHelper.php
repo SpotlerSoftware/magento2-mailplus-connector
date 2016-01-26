b9c2b4891da0e3e085f5d261043573186e1cc5ea
Paul Bosselaar <paul@mailplus.nl>
2016-01-26 13:11:26 +0100

    MAG-10: Add reviewLink and ratingValue to product data

--- MailPlus/Helper/MailPlusApi/ProductHelper.php
+++ MailPlus/Helper/MailPlusApi/ProductHelper.php
@@ -7,15 +7,27 @@ use Magento\Framework\App\Helper\AbstractHelper;
 class ProductHelper extends AbstractHelper {
 	
 	/**
-	 * 
-	 * @var \Magento\Framework\UrlInterface $_urlBuilder
+	 * @var \Magento\Framework\UrlInterface
 	 */
 	protected $_urlBuilder;
 	
-	public function __construct(\Magento\Framework\App\Helper\Context $context, \Magento\Framework\UrlInterface $urlBuilder) {
+	/** 
+	 * @var \Magento\Review\Model\RatingFactory
+	 */
+	protected $_ratingFactory;
+	
+	/**
+	 * @param \Magento\Framework\App\Helper\Context $context
+	 * @param \Magento\Framework\UrlInterface $urlBuilder
+	 * @param \Magento\Review\Model\RatingFactory $ratingFactory
+	 */
+	public function __construct(\Magento\Framework\App\Helper\Context $context,
+			\Magento\Framework\UrlInterface $urlBuilder,
+			\Magento\Review\Model\RatingFactory $ratingFactory) {
 		parent::__construct($context);
+
 		$this->_urlBuilder = $urlBuilder;
-		//$this->_urlBuilder->setUseSession(false);
+		$this->_ratingFactory = $ratingFactory;
 	}
 	
 	/**
@@ -25,6 +37,14 @@ class ProductHelper extends AbstractHelper {
 		return $product->getStoreId () . '-' . $product->getId ();
 	}
 	
+	public function getReviewUrl($product) {
+		return $this->_urlBuilder->getUrl('review/product/list', [
+						'id' => $product->getId(),
+						'category' => $product->getCategoryId()
+		]);
+		
+	}
+	
 	public function getProductData($product) {
 		
 		$externalId = $this->getExternalId($product);
@@ -32,8 +52,6 @@ class ProductHelper extends AbstractHelper {
 		if (empty($description)) {
 			$description = $product->getName();
 		}
-		
-		$categoryPaths = array(); // TODO
 		
 		$imageUrl = $this->_urlBuilder->getUrl('mailplus/image/get/', array('id' => $externalId, 'f' => 'n'));
 		$imageLargeUrl = $this->_urlBuilder->getUrl('mailplus/image/get/', array('id' => $externalId, 'f' => 'l'));
@@ -45,6 +63,14 @@ class ProductHelper extends AbstractHelper {
 		$visible = \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED == $product->getStatus() &&
 			 \Magento\Catalog\Model\Product\Visibility::VISIBILITY_NOT_VISIBLE != $product->getVisilibity();
 		
+		 /*
+		  * TODO:
+		  *  - specifications
+		  *  - category 
+		  */
+		
+		$categoryPaths = array(); // TODO
+			 
 		$data = array (
 				'update' => TRUE,
 				'product' => array(
@@ -62,9 +88,17 @@ class ProductHelper extends AbstractHelper {
 						'imageLargeUrl' => $imageLargeUrl,
 						'addToCartLink' => '',
 						'language' => $storeName,
-						'visible' => ($visible ? "true" : "false") 
+						'visible' => ($visible ? "true" : "false"),
+						'reviewLink' => $this->getReviewUrl($product)
 				) 
 		);
+		
+		$ratingSummary = $this->_ratingFactory->create()->getEntitySummary($product->getId());
+		if ($ratingSummary->getSum() > 0) {
+			$ratingPerc = $ratingSummary->getSum() / $ratingSummary->getCount();
+			// Normalize to 0-5
+			$data['product']['ratingValue'] = 5 * ($ratingPerc / 100); 
+		}
 		
 		if ($data['product']['price'] == $data['product']['oldPrice']) {
 			unset($data['product']['oldPrice']);
