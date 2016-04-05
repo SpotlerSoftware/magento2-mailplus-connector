05746159da4b096dbb14eac3c58976cadeb4f109
Frank van Schie <frank@mailplus.nl>
2016-04-05 14:45:42 +0200

    CP-93: Enable filtering for newsletter subscribers.

--- MailPlus/Model/SubscriberRepository.php
+++ MailPlus/Model/SubscriberRepository.php
@@ -1,8 +1,10 @@
 <?php
 namespace MailPlus\MailPlus\Model;
 
+use Magento\Framework\Api\Search\FilterGroup;
 use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\Framework\Api\SearchResultsFactory;
+use Magento\Newsletter\Model\ResourceModel\Subscriber\Collection;
 use Magento\Newsletter\Model\ResourceModel\Subscriber\CollectionFactory;
 use MailPlus\MailPlus\Api\SubscriberRepositoryInterface;
 
@@ -25,15 +27,19 @@ class SubscriberRepository implements SubscriberRepositoryInterface
 
     public function getList(SearchCriteriaInterface $searchCriteria)
     {
-        /** @var \Magento\Newsletter\Model\ResourceModel\Subscriber\Collection $collection */
+        /** @var Collection $collection */
         $collection = $this->_subscriberCollectionFactory->create();
+
+        foreach ($searchCriteria->getFilterGroups() as $group) {
+            $this->addFilterGroupToCollection($group, $collection);
+        }
+
         $collection
             ->addSubscriberTypeField()
             ->showStoreInfo()
             ->setPageSize($searchCriteria->getPageSize())
             ->setCurPage($searchCriteria->getCurrentPage())
             ->load();
-
 
         /** @var \Magento\Framework\Api\SearchResults $searchResult */
         $searchResult = $this->_searchResultsFactory->create();
@@ -43,4 +49,30 @@ class SubscriberRepository implements SubscriberRepositoryInterface
 
         return $searchResult;
     }
+
+    /**
+     * Helper function that adds a FilterGroup to the collection.
+     *
+     * @param FilterGroup $filterGroup
+     * @param Collection $collection
+     * @return void
+     */
+    protected function addFilterGroupToCollection(
+        FilterGroup $filterGroup,
+        Collection $collection
+    ) {
+        $fields = [];
+        $conditions = [];
+        foreach ($filterGroup->getFilters() as $filter) {
+            $fields[] = $filter->getField();
+            $conditionType = $filter->getConditionType() ? $filter->getConditionType() : 'eq';
+            $conditionValue = $conditionType != 'in' && $conditionType != 'nin' ? $filter->getValue() : explode(',', $filter->getValue());
+            $conditions[] = [ $conditionType => $conditionValue ];
+        }
+
+        if ($fields) {
+            $collection->addFieldToFilter($fields, $conditions);
+        }
+    }
+
 }
