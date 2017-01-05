2973e924cd5195e41815e22e0a2236e746e66013
bob <bob@mailplus.nl>
2017-01-05 11:22:12 +0100

    CP-695: Formatting

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -20,35 +20,35 @@ class UpgradeSchema implements UpgradeSchemaInterface
     {
         $setup->startSetup();
         if (version_compare($context->getVersion(), '1.0.1') < 0) {
-        $quoteConversionTable = $setup->getConnection()
-            ->newTable('mp_quote_conversion')
-            ->addColumn(
-                'mailplus_id',
-                Table::TYPE_TEXT,
-                255,
-                [],
-                'Mailplus ID'
-            )
-            ->addColumn(
-                'quote_id',
-                Table::TYPE_INTEGER,
-                10,
-                [
-                    'unsigned' => true,
-                    'nullable' => false,
-                    'primary' => true
-                ],
-                'Quote ID'
-            )
-            ->addIndex(
-                $setup->getIdxName('mp_quote_conversion', ['quote_id']),
-                ['quote_id']
-            )
-            ->addForeignKey('conversion_quote',
-                'quote_id',
-                'quote',
-                'entity_id');
-        $setup->getConnection()->createTable($quoteConversionTable);
+            $quoteConversionTable = $setup->getConnection()
+                ->newTable('mp_quote_conversion')
+                ->addColumn(
+                    'mailplus_id',
+                    Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'Mailplus ID'
+                )
+                ->addColumn(
+                    'quote_id',
+                    Table::TYPE_INTEGER,
+                    10,
+                    [
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'Quote ID'
+                )
+                ->addIndex(
+                    $setup->getIdxName('mp_quote_conversion', ['quote_id']),
+                    ['quote_id']
+                )
+                ->addForeignKey('conversion_quote',
+                    'quote_id',
+                    'quote',
+                    'entity_id');
+            $setup->getConnection()->createTable($quoteConversionTable);
 
 
         }
