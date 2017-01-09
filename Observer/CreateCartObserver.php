ab5d52c15bd6b7a37b7552095580c1cb68527e01
bob <bob@mailplus.nl>
2017-01-09 14:46:02 +0100

    CP-695: Changed the mpid capture method to use javascript and cookies. Magento was caching pages so the events were not firing

--- Observer/CreateCartObserver.php
+++ Observer/CreateCartObserver.php
@@ -4,6 +4,7 @@ namespace MailPlus\MailPlus\Observer;
 use Magento\Customer\Model\Session;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Stdlib\Cookie\PhpCookieManager;
 use Magento\Quote\Model\Quote;
 use MailPlus\MailPlus\Model\QuoteConversion;
 use MailPlus\MailPlus\Model\QuoteConversionFactory;
@@ -11,28 +12,28 @@ use MailPlus\MailPlus\Model\QuoteConversionFactory;
 class CreateCartObserver implements ObserverInterface
 {
     private $quoteConversionFactory;
-    private $sessionManager;
+    private $cookieManager;
 
     /**
      * CreateCartObserver constructor.
-     * @param Session $customerSession
+     * @param Session $cookieManager
      * @param QuoteConversionFactory $quoteConversionFactory
      * @internal param PhpCookieManager $cookieManager
      */
-    public function __construct(Session $customerSession, QuoteConversionFactory $quoteConversionFactory)
+    public function __construct(PhpCookieManager $cookieManager, QuoteConversionFactory $quoteConversionFactory)
     {
         $this->quoteConversionFactory = $quoteConversionFactory;
-        $this->sessionManager = $customerSession;
+        $this->cookieManager = $cookieManager;
     }
 
     public function execute(Observer $observer)
     {
-        $mpId = $this->sessionManager->getMpId();
+        $mpId = $this->cookieManager->getCookie("mpId");
         if(!isset($mpId)){
             return;
         }
         //Only use mpId for one cart creation
-        $this->sessionManager->setMpId(null);
+        $this->cookieManager->deleteCookie("mpId");
         /** @var Quote $quote */
         $quote = $observer->getData('quote');
 
