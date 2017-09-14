ac996d534ea706a3b94a253cd70c02b920a7bb4d
bob <bob@mailplus.nl>
2017-09-14 10:52:42 +0200

    CP-874: Added final_price to the magento product api and the connector platform field list

---
+++ Plugin/ProductRepositoryPlugin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MailPlus\MailPlus\Plugin;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
+class ProductRepositoryPlugin
+{
+
+
+    /**
+     * @param ProductRepositoryInterface $subject
+     * @param \Magento\Catalog\Api\Data\ProductSearchResultsInterface $result
+     * @return \Magento\Catalog\Api\Data\ProductSearchResultsInterface
+     */
+    public function afterGetList(ProductRepositoryInterface $subject, $result)
+    {
+        /** @var \Magento\Catalog\Model\Product $item */
+        foreach ($result->getItems() as $item) {
+            $finalPrice = $item->getFinalPrice();
+            $extensionAttributes = $item->getExtensionAttributes();
+            $extensionAttributes->setFinalPrice($finalPrice);
+            $item->setExtensionAttributes($extensionAttributes);
+        }
+        return $result;
+    }
+}
